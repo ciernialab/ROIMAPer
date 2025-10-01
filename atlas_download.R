f80606208ae57c28790e95cf73486bd96b05d118
JulianRode <julian.rodefeld@gmail.com>
2025-10-01 11:16:59 -0700

    correct atlas order

--- atlas_download.R
+++ atlas_download.R
@@ -6,18 +6,18 @@ destination <-  "ABA_API/"
 dir.create(destination)
 dir.create(paste0(destination, "coronal/"))
 dir.create(paste0(destination, "sagittal/"))
-slice_ids_coronal <- unlist(read.csv("http://api.brain-map.org/api/v2/data/query.csv?criteria=+model::AtlasImage,rma::criteria,atlas_data_set(atlases[id$eq1]),graphic_objects(graphic_group_label[id$eq159226751]),rma::options[tabular$eq'sub_images.id'][order$eq'sub_images.id']+&num_rows=all&start_row=0"))
-slice_ids_sagittal <- unlist(read.csv("http://api.brain-map.org/api/v2/data/query.csv?criteria=+model::AtlasImage,rma::criteria,atlas_data_set(atlases[id$eq2]),graphic_objects(graphic_group_label[id$eq28]),rma::options[tabular$eq'sub_images.id'][order$eq'sub_images.id']+&num_rows=all&start_row=0"))
+slice_ids_coronal <- unlist(read.csv("http://api.brain-map.org/api/v2/data/query.csv?criteria=+model::AtlasImage,rma::criteria,atlas_data_set(atlases[id$eq1]),graphic_objects(graphic_group_label[id$eq28]),rma::options[tabular$eq%27sub_images.id%27][order$eq%27sub_images.section_number%27]+&num_rows=all&start_row=0"))
+slice_ids_sagittal <- unlist(read.csv("http://api.brain-map.org/api/v2/data/query.csv?criteria=+model::AtlasImage,rma::criteria,atlas_data_set(atlases[id$eq2]),graphic_objects(graphic_group_label[id$eq28]),rma::options[tabular$eq%27sub_images.id%27][order$eq%27sub_images.section_number%27]+&num_rows=all&start_row=0"))
 
 save_and_modify_svgs <- function(urls, prefix, suffix, destination, name, width, height) {
-  for (i in 1:2) {
+  for (i in 1:length(urls)) {
     url <-  paste0(prefix, urls[i], suffix)
-    path <- paste0(destination, name, i, ".svg")
+    path <- paste0(destination, name, i, "_", urls[i], ".svg")
     download.file(url, destfile = path, mode = "wb")
-    svg_string <- readLines(paste0(destination, name, i, ".svg"))
+    svg_string <- readLines(path, warn = FALSE)
     svg_string <- stringr::str_replace_all(svg_string, "stroke:black", "stroke:none")
     writeLines(svg_string,path)
-    rsvg::rsvg_png(path, paste0(destination, name, i,".png"), width, height)
+    rsvg::rsvg_png(path, paste0(destination, name, i, "_", urls[i], ".png"), width, height)
     file.remove(path)
   }
 }
