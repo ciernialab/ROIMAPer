76472de267b3b84fb71be4f0d85302c14aab86bd
JulianRode <julian.rodefeld@gmail.com>
2026-01-03 16:11:03 -0800

    update allen atlas, python script now handles all conversion

--- ROIMAPerUtilities/allen_atlas_float_conversion.py
+++ ROIMAPerUtilities/allen_atlas_float_conversion.py
@@ -11,42 +11,67 @@ import matplotlib as mpl
 
 import numpy as np
 
-reader = sitk.ImageFileReader()
-reader.SetFileName("ROIMAPer/atlases/annotation_10.nrrd")
-image_adult = reader.Execute()
-
-reader.SetFileName("ROIMAPer/atlases/annotation_10_p56.nrrd")
-image_p56 = reader.Execute()
-
 def float_conversion(image):
     
-    #turn every pixel into its modulo 10000
+    #turn every pixel into its modulo 100000
     pixels = sitk.GetArrayFromImage(image)
-    replaced_pixels = pixels % 10000
+    pixels = pixels.astype(int)
+    replaced_pixels = pixels % 100000
     replaced_image = sitk.GetImageFromArray(replaced_pixels)
+    #change datatype to 32 bit float so it can be saved as tif
+    replaced_image = sitk.Cast(replaced_image, sitk.sitkFloat32)
     return replaced_image
-
-image_adult = float_conversion(image_adult)
-
-image_p56 = float_conversion(image_p56)
 
 def slicing(image):
     #first is width, second is height, third is depth
     #default orientation is sagittal
-    coronal = image[::10,:,:]
+    coronal = image[3::10,:,:]
     #now flip the coronal version
-    sagittal = image[:,:,::10]
-    return coronal, sagittal
-adult_coronal, adult_sagittal = slicing(image_adult)
-p56_coronal, p56_sagittal = slicing(image_p56)
+    coronal = sitk.PermuteAxes(coronal, order = [2,1,0])
+    #cut x axis in half for halfbrain
+    coronal_half = coronal[570:1140,:,:]
+    #use 20 um spacing for sagittal
+    sagittal = image[:,:,:570:20]
+    return coronal, sagittal, coronal_half
 
+reader = sitk.ImageFileReader()
+reader.SetFileName("ROIMAPer/atlases/annotation_10.nrrd")
+image_adult = reader.Execute()
+adult_coronal, adult_sagittal, adult_coronal_half = slicing(image_adult)
+del image_adult
+adult_coronal = float_conversion(adult_coronal)
+adult_sagittal = float_conversion(adult_sagittal)
+adult_coronal_half = float_conversion(adult_coronal_half)
 
 writer = sitk.ImageFileWriter()
-writer.SetFileName("ROIMAPer/atlases/adult_coronal.nrrd")
+writer.UseCompressionOn()
+writer.SetImageIO("TIFFImageIO")
+#deflate is lossless
+writer.SetCompressor("Deflate")
+writer.SetFileName("ROIMAPer/atlases/aba_v3_adult-Coronal.tif")
 writer.Execute(adult_coronal)
-writer.SetFileName("ROIMAPer/atlases/adult_sagittal.nrrd")
+writer.SetFileName("ROIMAPer/atlases/aba_v3_adult-Coronal_halfbrain.tif")
+writer.Execute(adult_coronal_half)
+writer.SetFileName("ROIMAPer/atlases/aba_v3_adult-Sagittal.tif")
 writer.Execute(adult_sagittal)
-writer.SetFileName("ROIMAPer/atlases/p56_coronal.nrrd")
+
+del adult_sagittal
+del adult_coronal
+
+reader.SetFileName("ROIMAPer/atlases/annotation_10_p56.nrrd")
+image_p56 = reader.Execute()
+p56_coronal, p56_sagittal, p56_coronal_half = slicing(image_p56)
+p56_coronal = float_conversion(p56_coronal)
+p56_sagittal = float_conversion(p56_sagittal)
+p56_coronal_half = float_conversion(p56_coronal_half)
+
+del image_p56
+
+writer.SetFileName("ROIMAPer/atlases/aba_v3_p56-Coronal.tif")
 writer.Execute(p56_coronal)
-writer.SetFileName("ROIMAPer/atlases/p56_sagittal.nrrd")
+writer.SetFileName("ROIMAPer/atlases/aba_v3_p56-Coronal_halfbrain.tif")
+writer.Execute(p56_coronal_half)
+writer.SetFileName("ROIMAPer/atlases/aba_v3_p56-Sagittal.tif")
 writer.Execute(p56_sagittal)
+del p56_sagittal
+del p56_coronal
